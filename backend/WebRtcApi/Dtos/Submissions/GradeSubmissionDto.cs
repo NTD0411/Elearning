f709bf8976120b457e691f2ab07c564156f7cca8
NTD0411 <tiendungn110@gmail.com>
2025-10-09 09:37:28 +0700

    Fix feature grade

--- backend/WebRtcApi/Dtos/Submissions/GradeSubmissionDto.cs
+++ backend/WebRtcApi/Dtos/Submissions/GradeSubmissionDto.cs
@@ -5,4 +5,5 @@ public class GradeSubmissionDto
     public decimal MentorScore { get; set; }
     public string FeedbackContent { get; set; } = string.Empty;
     public string Status { get; set; } = "Graded";
+    public int MentorId { get; set; }
 }
